dbb16b4afc44eccd5d9e186b0cc09849c6eef5e3
tawaschoolaccount <24121@karamu.nz>
2026-06-09 17:47:46 +1200

    added fade js and fixed a small typo

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const fadeSections = document.querySelectorAll(
+    '.fade-section, .fade-left, .fade-right, .fade-up, .fade-down, .fade-in'
+  );
+  const appearOptions = {
+    threshold: 0.1,
+    rootMargin: "0px 0px -50px 0px"
+  };
+
+  const appearOnScroll = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) return;
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
+    });
+  }, appearOptions);
+  fadeSections.forEach(section => appearOnScroll.observe(section));
+
 const sections = document.querySelectorAll("section");
 const navLinks = document.querySelectorAll(".nav-link");
 
@@ -13,3 +32,4 @@ const observer = new IntersectionObserver((entries) => {
 }, { threshold: 0.6 });
 
 sections.forEach((section) => observer.observe(section));
+}); 
